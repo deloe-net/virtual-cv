9023ff5b93b614a80f15ddc77ffa013cfc0be0e1
Ismael Lugo <12010989+IsmaelRLG@users.noreply.github.com>
2021-11-01 21:20:44 -0300

    added missing unittest

--- test/common/test_locales.py
+++ test/common/test_locales.py
@@ -1,3 +1,17 @@
+#  Copyright 2021 Ismael Lugo <ismael.lugo@deloe.net>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import os
 import unittest
 from unittest import mock
 
@@ -43,5 +57,62 @@ class TestLocales(unittest.TestCase):
         assert isinstance(o._cache, dict)
         assert o._lang_class is _default_lang_obj
 
+        c = mock.MagicMock()
+        o = Locales(p, c)
+        assert o._lang_class is c
 
-__all__ = ['TestLang']
+    def test_get_abspath(self):
+        p = '/test/absolute/path'
+        n = 'relative/route'
+        o = Locales(p)
+        r = o.get_abspath(n)
+        assert r == os.path.join(p, n)
+        assert os.path.isabs(r)
+
+    def test_cache(self):
+        o = Locales('./')
+        k = 'test-key'
+        v = mock.MagicMock()
+        assert o.set_cache(k, v) is None
+        assert o.validate_cache(k)
+        assert o.get_cache(k) is v
+
+    def test_pre_load(self):
+        v = 'return-value'
+        c = mock.MagicMock(return_value=v)
+        r = {'unique-key': 'awesome value'}
+        f = 'test-filename.txt'
+
+        o = Locales('./', c)
+        o.read = mock.MagicMock(return_value=r)
+        assert o._load(f) == v
+        o.read.assert_called_once_with(f)
+        c.assert_called_once_with(r)
+
+    @mock.patch('builtins.open',
+                new_callable=mock.mock_open,
+                read_data='{"mock-json": "some-value"}')
+    def test_read(self, mock_file):
+        f = '/test/filename.json'
+        r = Locales.read(f)
+        mock_file.assert_called_with(f)
+        self.assertDictEqual(r, {'mock-json': 'some-value'})
+
+    def test_load(self):
+        v = 'unique-value-obj'
+        p = '/test/path'
+        f = 'lang-filename.json'
+        d = os.path.join(p, f)
+        m = mock.MagicMock()
+
+        o = Locales(p, m)
+        o.get_cache = mock.MagicMock()
+        o.set_cache = mock.MagicMock()
+        o._load = mock.MagicMock(return_value=v)
+        r = o.load(f)
+        o.get_cache.assert_not_called()
+        o.set_cache.assert_called_once_with(d, v)
+        assert r is v
+
+
+__all__ = ['TestLang', 'TestLocales']
