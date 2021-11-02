04e6d2c0c3fc394cc404978d1e65bdd06b52a905
Ismael Lugo <12010989+IsmaelRLG@users.noreply.github.com>
2021-11-01 21:17:07 -0300

    improved docstrings

--- webapp/assets.py
+++ webapp/assets.py
@@ -16,7 +16,6 @@ import hashlib
 import os
 import re
 import shutil
-import typing
 
 from .common import Reactor
 from .logger import logger
@@ -30,14 +29,26 @@ hash_stack = {}
 
 
 class Assets:
+    """
+    Create safe file names, making it more hard to map static files like style
+    sheets, javascript files, etc.
+
+    :param algm: Name of the hash algorithm, used to secure the files.
+    :param salt: Unique and secret key used to secure files.
+
+    """
     STATIC_FOLDER = core.static_folder
     REGEX_REPLACE = re.compile('[^a-zA-Z]')
     FILE_FORMATTER = '{basename}.{hashsum}{ext}'
 
     def __init__(self, algm: str, salt: bytes = None):
+        """
+        Initialize the object.
+        """
+
         self.__salt = salt
         self._algm = algm
-        self.__data = {}
+        self.__cache = {}
 
     @property
     def static_folder(self):
@@ -51,16 +62,47 @@ class Assets:
     def regex_replace(self):
         return self.REGEX_REPLACE
 
-    def update_salt(self, salt: typing.Union[str, bytes] = None):
+    def update_salt(self, salt: bytes) -> None:
+        """
+        Update the unique key used to secure the files.
+
+        :param salt: Unique and secret key used to secure files.
+        """
+        if isinstance(salt, str):
+            salt = bytes(salt, 'utf-8')
         self.__salt = salt
 
-    def get_abspath(self, filename: str):
+    def get_abspath(self, filename: str) -> str:
+        """
+        Returns the absolute path of a file in the static folder.
+
+        :param filename: Relative path of the file in the folder
+        :return: Returns the absolute path of the file
+        """
         return os.path.join(self.static_folder, filename)
 
     def format(self, **kwargs):
+        """
+        Create safe filenames according to file format.
+
+        :param kwargs: The keyword arguments are passed to the str.format()
+            method, the available arguments are::
+
+            - basename: Base filename
+            - hashsum: File hash identifier
+            - ext: Extension used in the file
+
+        :return: returns the end name of the secured file
+        """
         return self.file_formatter.format(**kwargs)
 
     def digest(self, data: bytes):
+        """
+        Generate the hash identifier from the supplied data.
+
+        :param data: supplied data to generate the hash identifier
+        :return: returns the generated hash identifier
+        """
         hashsum = hashlib.pbkdf2_hmac(self._algm, data, self.__salt, 1000)
         hashsum = base64.b64encode(hashsum)
         if isinstance(hashsum, bytes):
@@ -68,13 +110,30 @@ class Assets:
         return self.regex_replace.sub('', hashsum)
 
     def get_file_hash(self, filename: str):
+        """
+        Generates a hash identifier from the name of a supplied file.
+
+        :param filename: File name to generate the hash identifier
+        :return: returns the generated hash identifier
+        """
         return self.digest(bytes(filename, 'utf-8'))
 
     def secure_filename(self, filename: str):
-        if filename in self.__data:
-            hashsum = self.__data[filename]
+        """
+        Returns the secure name for a file according to the path. This method
+        has the feature that in case the file name has been previously
+        processed, it verifies that it is available in the cache memory. If
+        found, the value is returned from cache, otherwise, it is processed,
+        and added to the cache.
+
+        :param filename: path where the file is located
+        :return: returns the safe filename.
+        """
+
+        if filename in self.__cache:
+            hashsum = self.__cache[filename]
         else:
-            hashsum = self.__data[filename] = self.get_file_hash(filename)
+            hashsum = self.__cache[filename] = self.get_file_hash(filename)
 
         dirname = os.path.dirname(filename)
         fn_parts = os.path.splitext(os.path.basename(filename))
